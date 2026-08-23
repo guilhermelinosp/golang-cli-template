0bf03e196cf5c6061d018139824d0167c41ceb22
guilhermelinosp <guilhermelinosp@users.noreply.github.com>
2026-08-23 11:40:17 -0300

    feat: add observability with hellnet-lib-telemetry

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,13 +5,26 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/guilhermelinosp/hellnet-lib-telemetry/telemetry"
 )
 
 func main() {
+	ops, err := telemetry.New(telemetry.Options{
+		ServiceName: "golang-cli-template",
+		Enabled:     os.Getenv("HELLNET_TELEMETRY_ENABLED") == "true",
+	})
+	if err != nil {
+		log.Fatalf("failed to init telemetry: %v", err)
+	}
+	defer func() { _ = ops.Shutdown() }()
+
 	var name string
 	flag.StringVar(&name, "name", "world", "name to greet")
 	flag.Parse()
 
+	ops.Log().Info("greeting", "name", name)
 	_, _ = fmt.Fprintf(os.Stdout, "hello %s\n", name)
 }
